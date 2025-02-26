fa3d88c148043bc7bcb02b3d88a485e3aea9ed8a
Bilal143260 <smbilal642@gmail.com>
2025-02-26 11:56:55 +0000

    Update model name and dataset parameters in training script; adjust training and validation steps

--- train_model.sh
+++ train_model.sh
@@ -3,12 +3,16 @@ export SOURCE_COLUMN="ghost_images"
 export TARGET_COLUMN="target"
 export MASK_COLUMN="binary_mask"
 export CAPTION_COLUMN="prompt"
-export MODEL_NAME="black-forest-labs/FLUX.1-dev"
+export MODEL_NAME="black-forest-labs/FLUX.1-Fill-dev"
+export TRAIN_DATASET_NAME="raresense/BGData"
+export TEST_DATASET_NAME="raresense/BGData_Validation"
 export OUTPUT_DIR="trained-flux-inpaint"
 
-accelerate launch --config_file accelerate_config.yaml train.py \
+accelerate launch train.py \
   --pretrained_model_name_or_path=$MODEL_NAME  \
   --output_dir=$OUTPUT_DIR \
+  --train_dataset_name=$TRAIN_DATASET_NAME \
+  --test_dataset_name=$TEST_DATASET_NAME \
   --source_column=$SOURCE_COLUMN \
   --target_column=$TARGET_COLUMN \
   --mask_column=$MASK_COLUMN \
@@ -22,13 +26,13 @@ accelerate launch --config_file accelerate_config.yaml train.py \
   --learning_rate=1e-5 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=100000 \
-  --validation_steps=500 \
+  --max_train_steps=10 \
+  --validation_steps=5 \
   --seed="42" \
   --height=768 \
   --width=576 \
   --max_sequence_length=512  \
-  --checkpointing_steps=1000  \
+  --checkpointing_steps=5  \
   --report_to="wandb" \
   --train_base_model \
   # --resume_from_checkpoint="latest"  \
